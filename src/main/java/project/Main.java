753570e719b03fe9eddbd24aaf9d6f962fdd83fd
adwansyed <adwan.syed@uoit.net>
2017-04-08 16:37:27 -0400

    Changed Title

--- src/main/java/project/Main.java
+++ src/main/java/project/Main.java
@@ -17,7 +17,7 @@ public class Main extends Application {
         IntroSplash splash = new IntroSplash(5000);
         splash.showSplash();
         Parent root = FXMLLoader.load(getClass().getResource("/fxml/main.fxml"));
-        primaryStage.setTitle("Neo Blackboard");
+        primaryStage.setTitle("Welcome");
         primaryStage.setScene(new Scene(root, 900, 700));
         primaryStage.show();
         primaryStage.setOnCloseRequest(e -> Platform.exit());
