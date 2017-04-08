2f3fa6774dc926b691c637bfe52b66bccbc743c3
adwansyed <adwan.syed@uoit.net>
2017-04-08 16:38:48 -0400

    Accelerator for Exit MenuItem

--- src/main/java/project/MainController.java
+++ src/main/java/project/MainController.java
@@ -1,9 +1,17 @@
 package project;
 
 import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.MenuItem;
+import javafx.scene.input.KeyCombination;
 
 public class MainController {
+    @FXML
+    private MenuItem exit;
 
+    public void initialize(){
+        exit.setAccelerator(KeyCombination.keyCombination("Ctrl+Q"));
+    }
     public void exit(ActionEvent actionEvent) {
         System.exit(0);
     }
