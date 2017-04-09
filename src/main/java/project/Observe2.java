9a0dc1b7439f99debeedee5d1b04170887bc58da
Andrew Selvarajah <selvarajah.andrew@gmail.com>
2017-04-09 15:57:58 -0400

    Added Node class, TexAreaTableCell class and updated Calendercontroller and fxm

---
+++ src/main/java/project/Observe2.java
@@ -0,0 +1,92 @@
+package project;
+
+
+/**
+ * Author(s): Adwan Syed, Andrew Selvarajah, Ahmed Naeem, Yi Guo
+ */
+public class Observe2 {
+    private String first;
+    private String second;
+    private String third;
+    private String fourth;
+    private String fifth;
+    private String sixth;
+    private String seventh;
+    private int id;
+
+    public Observe2( String first, String second, String third, String fourth, String fifth, String sixth, String seventh, int id){
+        this.first = first;
+        this.second = second;
+        this.third = third;
+        this.fourth = fourth;
+        this.fifth = fifth;
+        this.sixth = sixth;
+        this.seventh = seventh;
+        this.id = id;
+
+    }
+
+    public String getFirst() {
+        return first;
+    }
+
+    public void setFirst(String first) {
+        this.first = first;
+    }
+
+    public String getSecond() {
+        return second;
+    }
+
+    public void setSecond(String second) {
+        this.second = second;
+    }
+
+    public String getThird() {
+        return third;
+    }
+
+    public void setThird(String third) {
+        this.third = third;
+    }
+
+    public String getFourth() {
+        return fourth;
+    }
+
+    public void setFourth(String fourth) {
+        this.fourth = fourth;
+    }
+
+    public String getFifth() {
+        return fifth;
+    }
+
+    public void setFifth(String fifth) {
+        this.fifth = fifth;
+    }
+
+    public String getSixth() {
+        return sixth;
+    }
+
+    public void setSixth(String sixth) {
+        this.sixth = sixth;
+    }
+
+    public String getSeventh() {
+        return seventh;
+    }
+
+    public void setSeventh(String seventh) {
+        this.seventh = seventh;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+}
